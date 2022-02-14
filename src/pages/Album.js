12a64f508a55682c769c9889b1e0ebfd8e6add01
wine-user <joqlima5@gmail.com>
2022-02-14 12:59:36 -0300

    removed import did't use

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
 import './styles/Album.css';
